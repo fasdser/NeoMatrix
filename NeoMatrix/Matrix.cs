4747489b39a4c0cae3154199e4cd44e9a61e2ea2
fasdser <107477645+fasdser@users.noreply.github.com>
2022-07-28 19:30:48 +0300

    Add project files.

---
+++ NeoMatrix/Matrix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NeoMatrix
+{
+    class Matrix
+    {
+        static readonly object locker = new object();
+        Random random;
+        const string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        public int Colunm { get; set; }
+
+        public Matrix(int col)
+        {
+            Colunm = col;
+            random = new Random((int)DateTime.Now.Ticks);
+        }
+
+        private char GetChar()
+        {
+            return str.ToCharArray()[random.Next(0, 35)];
+        }
+
+        public void Move()
+        {
+            int lenght;
+            int count;
+            while (true)
+            {
+                count = random.Next(3, 6);
+                lenght = 0;
+                Thread.Sleep(random.Next(20, 5000));
+                for (int i = 0; i < 40; i++)
+                {
+                    lock (locker)
+                    {
+                        Console.CursorTop = 0;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        for (int j = 0; j < i; j++)
+                        {
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine("█");
+                        }
+                        if (lenght < count)
+                            lenght++;
+                        else
+                            if (lenght == count)
+                            count = 0;
+
+                        if (39 - i < lenght)
+                            lenght--;
+                        Console.CursorLeft = i - lenght + 1;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        for (int j = 0; j < lenght - 2; j++)
+                        {
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());
+                        }
+                        if (lenght >= 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());
+                        }
+                        if (lenght >= 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.CursorLeft = Colunm;
+                            Console.WriteLine(GetChar());
+                        }
+                        Thread.Sleep(20);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NeoMatrix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NeoMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Matrix matrix;
+            for (int i = 0; i < 60; i++)
+            {
+                matrix = new Matrix(i * 2);
+                new Thread(matrix.Move).Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
